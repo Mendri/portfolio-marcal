7928d27c59aa767bc9ebecd667b68ff0bf98f8fe
Mendri <marcalmendrighi@gmail.com>
2022-06-16 08:40:15 -0300

    Feito as estilizações finais para a home 1.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,34 +14,58 @@ class Home extends React.Component {
         <Header />
         <div id='abouts'>
           <div id='about-me'>
-            <h2
+            <div
               id='aboutMe-title'
               onMouseEnter={ () => aboutMeDispatch() }
               onMouseLeave={ () => aboutMeDispatch() }
             >
-              Quer saber um pouco sobre mim?
-            </h2>
-            {
+              <p>Quer saber um pouco sobre mim?</p>
+              {
               aboutMeState &&
               (
-                <div id='aboutMe-container'>Sou gostoso</div>
+                <div id='aboutMe-container'>
+                  <p>Buenas!!! Meu nome é Marçal, tenho 19 anos, moro em São Carlos e o Mendri no título é
+                  a junção dos sobrenomes de solteiro dos meus pais (Mendonça e Aldrighi).
+                  </p>
+                  <br />
+                  <p>
+                    Sou um estudante de desenvolvimento web pela trybe e já tenho conhecimento das seguintes tecnologias: HTML, CSS, JavaScript, React, Redux e Jest
+                  </p>
+                  <br />
+                  <p>
+                    Meu objetivo pessoal, pelo menos por enquanto é me tornar um programador competente
+                  </p>
+                </div>
               )
             }
+            </div>
           </div>
           <div id='about-portfolio'>
-            <h2
+            <div
             id='aboutPortfolio-title'
-              onMouseEnter={ () => aboutPortfolioDispatch() }
-              onMouseLeave={ () => aboutPortfolioDispatch() }
+            onMouseEnter={ () => aboutPortfolioDispatch() }
+            onMouseLeave={ () => aboutPortfolioDispatch() }
             >
-              Quer saber um pouco sobre esse Portfólio?
-            </h2>
+              <p>Quer saber um pouco sobre esse Portfólio?</p>
             {
               aboutPortfolioState &&
               (
-                <div id='aboutPortfolio-container'>Esse Portfólio também é gostoso</div>
+                <div id='aboutPortfolio-container'>
+                  <p>
+                    Pois bem, este portfólio obviamente foi feito para armazenar meus projetos pessoais, para que então eu pudesse reuní-los em um só lugar.
+                  </p>
+                  <br />
+                  <p>
+                    Mas ele também é especial, pois ele é o meu primeiro projeto pessoal com nenhum caráter acadêmico, já que decidi fazê-lo apenas como um treinamento
+                  </p>
+                  <br />
+                  <p>
+                    Pelo menos inicialmente haveram três caminhos neste projeto, este aqui que é home, feito para introduzir eu e este projeto a você. Há o caminho contatos que tem o propósito de apresentar as minha redes de comunicação e por fim e mais importante o caminho dos projetos, este contará com alguns projetos demonstrando as minhas habilidades em certas tecnologias.
+                  </p>
+                </div>
               )
             }
+            </div>
           </div>
         </div>
       </div>
