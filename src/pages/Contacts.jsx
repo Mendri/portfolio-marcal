60ad9620fe50afa645705250e47b00c65d1f6c9b
Mendri <marcalmendrighi@gmail.com>
2022-06-17 06:24:37 -0300

    Dando uma estilização para a página contatos, que pelo menos por hora esta bom

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 import Header from '../components/Header'
+import '../css/Contacts.css'
 
 function Contacts () {
   return (
-    <div>
+    <div id='contact'>
       <Header />
-      Contacts
+      <div id='contacts-container'>
+        <h2 id='contact-title'>Minhas redes de contato:</h2>
+        <div id='envelope-container'>
+          <p id='envelope-paragraph'>
+            Email principal:
+            marcalmendrighi@gmail.com
+          </p>
+          <FontAwesomeIcon id='envelope-icon' icon={faEnvelope} />
+        </div>
+        <div id='linkedin-container'>
+          <a href='https://www.linkedin.com/in/mendrighi/'>
+            <FontAwesomeIcon id='linkedin-icon' icon={faLinkedin} />
+          </a>
+          <a href='https://www.linkedin.com/in/mendrighi/' id='linkedin-paragraph'>
+            Caso queira uma abordagem mais profissional, este é meu Linkedin
+          </a>
+        </div>
+        <div id='github-container'>
+          <a id='github-paragraph' href='https://github.com/Mendri'>
+            Se quiser acompanhar os projetos que eu já participei, este é meu GitHub
+          </a>
+          <a href='https://github.com/Mendri'>
+            <FontAwesomeIcon id='github-icon' icon={faGithub} />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
