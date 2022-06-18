085dccd53138faecfda833ee3eaa345c19455dff
Mendri <marcalmendrighi@gmail.com>
2022-06-18 05:23:34 -0300

    Protótipo feito

---
+++ src/data/projectList.js
@@ -0,0 +1,32 @@
+const projectList = [
+  {
+    projectId: '1',
+    projectName: '',
+    projectImg: '',
+    projectLink: '',
+    projectAbout: ''
+  },
+  {
+    projectId: '2',
+    projectName: '',
+    projectImg: '',
+    projectLink: '',
+    projectAbout: ''
+  },
+  {
+    projectId: '3',
+    projectName: '',
+    projectImg: '',
+    projectLink: '',
+    projectAbout: ''
+  },
+  {
+    projectId: '4',
+    projectName: '',
+    projectImg: '',
+    projectLink: '',
+    projectAbout: ''
+  }
+]
+
+export default projectList
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
+import projectList from '../data/projectList'
 import Header from '../components/Header'
+import '../css/Projects.css'
 
 function Projects () {
   return (
-    <div>
+    <div id='projects'>
       <Header />
-      Projects
+      <div id='projects-container'>
+        <h2 id='projects-title'>Acervo de projetos</h2>
+        {
+          projectList.map((project) => {
+            return (
+            <div
+              key={project.projectId}
+              className='project-container'
+            >
+              <h3>Projeto {project.projectId}</h3>
+            </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
